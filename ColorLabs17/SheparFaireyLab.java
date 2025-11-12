5fc4a6360a9a39341bf4642c354691bf6899eccd
Zoey Tian <109251803+ztian1113@users.noreply.github.com>
2025-11-12 09:39:58 -0600

    lit

--- ColorLabs17/SheparFaireyLab.java
+++ ColorLabs17/SheparFaireyLab.java
@@ -11,40 +11,133 @@ import java.util.List; // resolves problem with java.awt.List and java.util.List
 
 public class SheparFaireyLab
 {
-    /**
-     * main method, to test the picture
-     *  
-     */
     public static void main(String[] args)
     {
+         Picture pic = new Picture("images/ShepardFairey.jpg");
+         //pic.explore();
+         
+         int red, green, blue, grey;
+         Pixel[] pixels = pic.getPixels();
+         
+         for (Pixel spot: pixels)
+         {
+             red = spot.getRed();
+             green = spot.getGreen();
+             blue = spot.getBlue();
+             grey = (red + green + blue)/3;
+             spot.setRed(grey);
+             spot.setGreen(grey);
+             spot.setBlue(grey);
+         }
+         pic.explore();
+         
+         /*
+         for (Pixel spot: pixels)
+         {
+             red = spot.getRed();
+             green = spot.getGreen();
+             blue = spot.getBlue();
+             grey = (red + green + blue)/3;
+             if (grey < 64)
+             {
+                 spot.setRed(0);
+                 spot.setGreen(0);
+                 spot.setBlue(100);
+             }
+             else if (grey < 128)
+             {
+                 spot.setRed(200);
+                 spot.setGreen(10);
+                 spot.setBlue(10);
+             }
+             else if (grey < 192)
+             {
+                 spot.setRed(175);
+                 spot.setGreen(215);
+                 spot.setBlue(250);
+             }
+             else
+             {
+                 spot.setRed(235);
+                 spot.setGreen(230);
+                 spot.setBlue(220);
+             }
+         }
+         pic.explore();
+         */
+         
+         /*
+         for (Pixel spot: pixels)
+         {
+             red = spot.getRed();
+             green = spot.getGreen();
+             blue = spot.getBlue();
+             grey = (red + green + blue)/3;
+             if (grey < 75)
+             {
+                 spot.setRed(0);
+                 spot.setGreen(0);
+                 spot.setBlue(100);
+             }
+             else if (grey < 135)
+             {
+                 spot.setRed(200);
+                 spot.setGreen(10);
+                 spot.setBlue(10);
+             }
+             else if (grey < 195)
+             {
+                 spot.setRed(175);
+                 spot.setGreen(215);
+                 spot.setBlue(250);
+             }
+             else
+             {
+                 spot.setRed(235);
+                 spot.setGreen(230);
+                 spot.setBlue(220);
+             }
+         }
+         pic.explore();
+         */
         
-         //opens selfie picture 
-          /**/
-         String fileName = FileChooser.pickAFile();
-         Picture pictObj = new Picture(fileName);
-         pictObj.explore();
-         
-         //relative path
-         Picture apic = new Picture("images\\beach.jpg");
-         //change with selfie picture
-         Picture me = new Picture("images/beach.jpg");
-         Picture me1 = new Picture("images/beach.jpg");
-         Picture me2 = new Picture("images/beach.jpg");
-         
-         /**
-          * method 1 change
-          * 
-          */
-         
-         /**
-          * method 2 change
-          * 
-          */
-         
-         /**
-          * custom color palette
-          */
-
-         
-    }//main       
-}//class
+         for (Pixel spot: pixels)
+         {
+             red = spot.getRed();
+             green = spot.getGreen();
+             blue = spot.getBlue();
+             grey = (red + green + blue)/3;
+             if (grey < 51)
+             {
+                 spot.setRed(50);
+                 spot.setGreen(19);
+                 spot.setBlue(37);
+             }
+             else if (grey < 102)
+             {
+                 spot.setRed(88);
+                 spot.setGreen(15);
+                 spot.setBlue(60);
+             }
+             else if (grey < 153)
+             {
+                 spot.setRed(174);
+                 spot.setGreen(29);
+                 spot.setBlue(55);
+             }
+             else if (grey < 204)
+             {
+                 spot.setRed(211);
+                 spot.setGreen(142);
+                 spot.setBlue(89);
+             }
+             else
+             {
+                 spot.setRed(255);
+                 spot.setGreen(246);
+                 spot.setBlue(208);
+             }
+         }
+         pic.explore();
+    }     
+}
