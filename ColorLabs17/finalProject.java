afe4ea735fd4d1bb15ed9bd963c81a5ef9fbb4be
Zoey Tian <109251803+ztian1113@users.noreply.github.com>
2026-05-12 11:49:00 -0500

    color stuff

--- ColorLabs17/finalProject.java
+++ ColorLabs17/finalProject.java
@@ -12,11 +12,19 @@ public class finalProject
     
     public static void main(String[] args)
     {
-        Picture cats = new Picture("images/cats.jpg");
+        Picture cats1 = new Picture("images/cats.jpg");
+        Picture cats2 = new Picture("images/cats.jpg");
+        Picture cats3 = new Picture("images/cats.jpg");
+        Picture cats4 = new Picture("images/cats.jpg");
+        Picture cats5 = new Picture("images/cats.jpg");
+        Picture cats6 = new Picture("images/cats.jpg");
+        Picture heartHands = new Picture("images/heartHands.jpg");
         Picture canvas = new Picture("images/canvas.jpg");
-        copytoCanvas(cats, canvas, 0, 0);
-        mirrorVertical(cats);
-        copytoCanvas(cats, canvas, cats.getWidth(), 0);
+        copytoCanvas(cats1, canvas, 0, 0);
+        mirrorVertical(cats2);
+        copytoCanvas(cats2, canvas, cats2.getWidth(), 0);
+        blackAndWhite(cats5);
+        copytoCanvas(cats5, canvas, 0, cats5.getHeight());
         canvas.explore();
     }
     
@@ -41,6 +49,85 @@ public class finalProject
                 rightPixel.setColor(leftPixel.getColor());
                 
             }
+        }
+    }
+    
+    public static void greyScale(Picture source)
+    {
+        int red, green, blue, grey;
+        Pixel[] pixels = source.getPixels();
+         
+        for (Pixel spot: pixels)
+        {
+            red = spot.getRed();
+            green = spot.getGreen();
+            blue = spot.getBlue();
+            grey = (red + green + blue)/3;
+            spot.setRed(grey);
+            spot.setGreen(grey);
+            spot.setBlue(grey);
+        }
+    }
+    
+    public static void blackAndWhite(Picture source)
+    {
+        int red, green, blue;
+        greyScale(source);
+        Pixel[] pixels = source.getPixels();
+        
+        for (Pixel spot: pixels)
+        {
+            if (spot.getRed() > 75)
+            {
+                spot.setRed(255);
+                spot.setGreen(255);
+                spot.setBlue(255);
+            }
+            else
+            {
+                spot.setRed(0);
+                spot.setGreen(0);
+                spot.setBlue(0);
+            }
+        }
+    }
+    
+    public static void invert(Picture source)
+    {
+        int red, green, blue;
+        Pixel[] pixels = source.getPixels();
+         
+        for (Pixel spot: pixels)
+        {
+            red = 255 - spot.getRed();
+            green = 255 - spot.getGreen();
+            blue = 255 - spot.getBlue();
+            spot.setRed(red);
+            spot.setGreen(green);
+            spot.setBlue(blue);
+        }
+    }
+    
+    public static void overlay(Picture source1, Picture source2)
+    {
+        int red1, green1, blue1, red2, green2, blue2;
+        Pixel[] pixels1 = source1.getPixels();
+        Pixel[] pixels2 = source2.getPixels();
+        
+        for (Pixel spot: pixels1)
+        {
+            red1 = spot.getRed();
+            green1 = spot.getGreen();
+            blue1 = spot.getBlue();
+        }
+        for (Pixel spot: pixels2)
+        {
+            red2 = spot.getRed();
+            green2 = spot.getGreen();
+            blue2 = spot.getBlue();
+            //spot.setRed((red1 + red2) / 2);
+            //spot.setGreen((green1 + green2) /2);
+            //spot.setBlue((blue1 + blue2) / 2);
         }
     }
     
